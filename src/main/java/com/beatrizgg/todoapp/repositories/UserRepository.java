3c5c70ef503f78bda3d5d93ed8140427c4ac801e
Beatriz <biabagg12@gmail.com>
2024-09-18 14:45:47 -0300

    Create UserDetailsServiceImpl and JWTUtil

--- src/main/java/com/beatrizgg/todoapp/repositories/UserRepository.java
+++ src/main/java/com/beatrizgg/todoapp/repositories/UserRepository.java
@@ -2,10 +2,14 @@ package com.beatrizgg.todoapp.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.beatrizgg.todoapp.models.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Transactional(readOnly = true)
+    User findByUsername(String username);
 
 }
