21317c58123da9eed8d37b9692dfbdc21ac96b42
Beatriz <biabagg12@gmail.com>
2024-09-18 14:35:20 -0300

    Add password encoder and create ProfileEnum

--- src/main/java/com/beatrizgg/todoapp/services/UserService.java
+++ src/main/java/com/beatrizgg/todoapp/services/UserService.java
@@ -1,18 +1,25 @@
 package com.beatrizgg.todoapp.services;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.beatrizgg.todoapp.models.User;
+import com.beatrizgg.todoapp.models.enums.ProfileEnum;
 import com.beatrizgg.todoapp.repositories.UserRepository;
 import com.beatrizgg.todoapp.services.exceptions.DataBindingViolationException;
 import com.beatrizgg.todoapp.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
     private UserRepository userRepository;
@@ -26,6 +33,8 @@ public class UserService {
     @Transactional
     public User create(User obj) {
         obj.setId(null);
+        obj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
+        obj.setProfiles(Stream.of(ProfileEnum.USER.getCode()).collect(Collectors.toSet()));
         obj = this.userRepository.save(obj);
         return obj;
     }
@@ -34,10 +43,11 @@ public class UserService {
     public User update(User obj) {
         User newObj = findById(obj.getId());
         newObj.setPassword(obj.getPassword());
+        newObj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
         return this.userRepository.save(newObj);
     }
 
-    public void delete(Long id) {
+    public void delete(Long id) {   
         findById(id);
         try {
             this.userRepository.deleteById(id);
