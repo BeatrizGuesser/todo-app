15f71eef7f4fd0f33b0044964671696e26df8e6a
Beatriz <biabagg12@gmail.com>
2024-09-20 14:50:39 -0300

    Add signup screen style

--- view/scripts/signup.js
+++ view/scripts/signup.js
@@ -4,29 +4,42 @@ async function signup() {
 
     console.log(username, password);
 
-    const response = await fetch("http://localhost:8080/user", {
-        method: "POST",
-        headers: new Headers({
-            "Content-Type": "application/json; charset=utf8",
-            Accept: "application/json",
-        }),
-        body: JSON.stringify({
-            username: username,
-            password: password,
-        }),
-    });
+    try {
 
-    if (response.ok) {
-        showToast("#okToast");
-    } else {
-        showToast("#errorToast");
+        const response = await fetch("http://localhost:8080/user", {
+            method: "POST",
+            headers: new Headers({
+                "Content-Type": "application/json; charset=utf8",
+                Accept: "application/json",
+            }),
+            body: JSON.stringify({
+                username: username,
+                password: password,
+            }),
+        });
+
+        if (response.ok) {
+            showToast("#okToast", "Successfully registered user.");
+        } else {
+            const errorData = await response.json();
+            const errorMessage = errorData.message || "Error when creating a new user.";
+            showToast("#errorToast", errorMessage);
+        }
+
+    } catch (error) {
+        console.error("Error:", error);
+        showToast("#errorToast", "An unexpected error occurred.");
     }
 }
 
-function showToast(id) {
-    var toastElList = [].slice.call(document.querySelectorAll(id));
-    var toastList = toastElList.map(function (toastEl) {
-        return new bootstrap.Toast(toastEl);
-    });
-    toastList.forEach((toast) => toast.show());
+function showToast(id, message) {
+    const toastElement = document.querySelector(id);
+    const toastBody = toastElement.querySelector('.toast-body');
+    toastBody.textContent = message;
+
+    toastElement.style.zIndex = "1056";
+    toastElement.style.position = "fixed";
+
+    const toast = new bootstrap.Toast(toastElement);
+    toast.show();
 }
