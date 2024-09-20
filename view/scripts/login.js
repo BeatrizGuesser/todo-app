f04bf8db52a636b733cc97850648fc60c88d4504
Beatriz <biabagg12@gmail.com>
2024-09-20 15:29:04 -0300

    Add login screen style

--- view/scripts/login.js
+++ view/scripts/login.js
@@ -1,40 +1,52 @@
 async function login() {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
-
+    
     console.log(username, password);
+    
+    try {
 
-    const response = await fetch("http://localhost:8080/login", {
-        method: "POST",
-        headers: new Headers({
-            "Content-Type": "application/json; charset=utf8",
-            Accept: "application/json",
-        }),
-        body: JSON.stringify({
-            username: username,
-            password: password,
-        }),
-    });
+        const response = await fetch("http://localhost:8080/login", {
+            method: "POST",
+            headers: new Headers({
+                "Content-Type": "application/json; charset=utf8",
+                Accept: "application/json",
+            }),
+            body: JSON.stringify({
+                username: username,
+                password: password,
+            }),
+        });
 
-    let key = "Authorization";
-    let token = response.headers.get(key);
-    window.localStorage.setItem(key, token);
+        let key = "Authorization";
+        let token = response.headers.get(key);
+        window.localStorage.setItem(key, token);
 
-    if (response.ok) {
-        showToast("#okToast");
-    } else {
-        showToast("#errorToast");
+        if (response.ok) {
+            showToast("#okToast", "Login successful. Redirecting...");
+            window.setTimeout(function () {
+                window.location = "/view/index.html";
+            }, 2000);
+        } else {
+            const errorData = await response.json();
+            const errorMessage = errorData.message || "Error when logging in.";
+            showToast("#errorToast", errorMessage);
+        }
+
+    } catch (error) {
+        console.error("Error:", error);
+        showToast("#errorToast", "An unexpected error occurred.");
     }
-
-    window.setTimeout(function () {
-        window.location = "/view/index.html";
-    }, 2000);
 }
 
-function showToast(id) {
-    var toastElList = [].slice.call(document.querySelectorAll(id));
-    var toastList = toastElList.map(function (toastEl) {
-        return new bootstrap.Toast(toastEl);
-    });
-    toastList.forEach((toast) => toast.show());
+function showToast(id, message) {
+    const toastElement = document.querySelector(id);
+    const toastBody = toastElement.querySelector('.toast-body');
+    toastBody.textContent = message;
+
+    toastElement.style.zIndex = "1056";
+    toastElement.style.position = "fixed";
+
+    const toast = new bootstrap.Toast(toastElement);
+    toast.show();
 }
