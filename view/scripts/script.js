b9c9677d8ef9925f408d9c8e515d4f1d72a9b3c2
Beatriz <biabagg12@gmail.com>
2024-09-23 11:12:57 -0300

    Add index screen style

--- view/scripts/script.js
+++ view/scripts/script.js
@@ -1,45 +1,65 @@
 const tasksEndpoint = "http://localhost:8080/task/user";
 
+// Loading
 function hideLoader() {
-  document.getElementById("loading").style.display = "none";
+    document.getElementById("loading").style.display = "none";
 }
 
+// Display Tasks
 function show(tasks) {
-  let tab = `<thead>
-            <th scope="col">#</th>
-            <th scope="col">Description</th>
-        </thead>`;
-
-  for (let task of tasks) {
-    tab += `
-            <tr>
-                <td scope="row">${task.id}</td>
-                <td>${task.description}</td>
-            </tr>
-        `;
-  }
-
-  document.getElementById("tasks").innerHTML = tab;
+    let postIts = '';
+    for (let task of tasks) {
+        postIts += `
+            <div class="col">
+                <div class="card h-100 bg-custom-gradi">
+                    <div class="card-body d-flex flex-column">
+                        <h5 class="card-title text-dark">Task ${task.id}</h5>
+                        <p class="card-text flex-grow-1">${task.description}</p>
+                        <div class="mt-auto">
+                            <button type="button" class="btn btn-light btn-sm me-2" onclick="getTask(${task.id})">
+                                Update <i class="bi bi-pencil"></i>
+                            </button>
+                            <button type="button" class="btn btn-success btn-sm" onclick="deleteTask(${task.id})">
+                                Done <i class="bi bi-check-circle"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+    }
+    document.getElementById("tasks").innerHTML = postIts;
 }
 
+// Display Tasks
 async function getTasks() {
-  let key = "Authorization";
-  const response = await fetch(tasksEndpoint, {
-    method: "GET",
-    headers: new Headers({
-      Authorization: localStorage.getItem(key),
-    }),
-  });
+    let key = "Authorization";
+    try {
+        const response = await fetch(tasksEndpoint, {
+            method: "GET",
+            headers: new Headers({
+                Authorization: localStorage.getItem(key),
+            }),
+        });
 
-  var data = await response.json();
-  console.log(data);
-  if (response) hideLoader();
-  show(data);
+        if (response.ok) {
+            var data = await response.json();
+            console.log("get tasks: " + data);
+            if (response) hideLoader();
+            show(data);
+        } else {
+            const errorData = await response.json();
+            const errorMessage = errorData.message || "Failed to fetch tasks.";
+            showToast("#errorToast", errorMessage);
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        showToast("#errorToast", "An unexpected error occurred while fetching tasks.");
+    }
 }
 
 document.addEventListener("DOMContentLoaded", function (event) {
-  if (!localStorage.getItem("Authorization"))
-    window.location = "/view/login.html";
+    if (!localStorage.getItem("Authorization"))
+        window.location = "/view/login.html";
 });
 
 getTasks();
